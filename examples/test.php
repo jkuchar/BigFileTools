eb6fb3edefc593c91729549b60bfba9075a1fd3d
Jan Kuchař <honza@mujserver.net>
2013-05-27 14:05:51 +0200

    - removed nette from example

--- examples/test.php
+++ examples/test.php
@@ -1,7 +1,6 @@
 <?php
 
-require 'nette/nette.min.php';
 require '../class/BigFileTools.php';
 
 
-echo $f = BigFileTools::fromPath("nette/nette.min.php")->getSize()." bytes";
+echo $f = BigFileTools::fromPath(__FILE__)->getSize()." bytes";
