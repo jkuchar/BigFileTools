3981080b0223a75663ca4102b4b71ef8a50ab213
Jan Kuchař <honza.kuchar@grifart.cz>
2016-04-27 19:08:52 +0200

    tests: file name with UTF-8

--- tests/setup-linux.sh
+++ tests/setup-linux.sh
@@ -8,6 +8,7 @@ bash tests/cleanup.sh
 
 touch tests/temp/emptyfile.tmp
 touch "tests/temp/empty - file.tmp"
+touch "tests/temp/emptyfileěšů指事字.tmp"
 truncate -s 1M tests/temp/smallfile.tmp
 truncate -s 2050M tests/temp/mediumfile.tmp
 truncate -s 4100M tests/temp/bigfile.tmp
